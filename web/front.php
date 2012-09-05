d5a26a2f8f6fe242516f37148dc38d074a219040
Dien Vu <meotimdihia@gmail.com>
2012-09-05 08:33:12 +0700

    continue

--- web/front.php
+++ web/front.php
@@ -5,25 +5,28 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
-$routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'World')));
-$routes->add('bye', new Routing\Route('/bye'));
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+ 
+    return new Response(ob_get_clean());
+}
+
+$routes = include __DIR__.'/../src/app.php';
 
 $request = Request::createFromGlobals();
 
 $context = new Routing\RequestContext();
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
- 
-    $response = new Response(ob_get_clean());
+ 	$request->attributes->add($matcher->match($request->getPathInfo()));
+	$response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
 } catch (Exception $e) {
     $response = new Response('An error occurred', 500);
 }
- 
  
 $response->send();
